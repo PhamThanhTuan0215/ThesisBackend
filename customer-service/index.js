9db362529d0a25acec66c6d49e6a4984f952e00e
PhamThanhTuan0215 <thanhtuancr1234@gmail.com>
2025-05-17 12:34:02 +0700

    init customer-service

---
+++ customer-service/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./database/sequelize')
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
+const {PORT} = process.env
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    return res.status(200).json({code: 0, message: 'Hello customer service'})
+})
+
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("Connect database server successfully")
+
+        sequelize.sync()
+            .then(() => {
+
+                console.log('Database synchronized');
+
+                app.listen(PORT || 3003, () => {
+                    console.log("http://localhost:" + (PORT || 3003));
+                });
+            })
+            .catch(err => {
+                console.log("Error syncing database:", err.message);
+                process.exit(1);
+            });
+    })
+    .catch(err => {
+        console.log("Can not connect database server: " + err.message)
+        process.exit(1);
+    });
