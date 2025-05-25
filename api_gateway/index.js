28ad1d8c82dba624dbb687f62096fd300eb1db54
PhamThanhTuan0215 <thanhtuancr1234@gmail.com>
2025-05-25 16:39:47 +0700

    add url discount-service, url order-service

--- api_gateway/index.js
+++ api_gateway/index.js
@@ -9,7 +9,9 @@ const {
     URL_API_GATEWAY,
     PORT, URL_PRODUCT_SERVICE,
     URL_USER_SERVICE,
-    URL_CUSTOMER_SERVICE
+    URL_CUSTOMER_SERVICE,
+    URL_DISCOUNT_SERVICE,
+    URL_ORDER_SERVICE,
     } = process.env
 
 app.use(cors());
@@ -22,6 +24,8 @@ app.get('/', (req, res) => {
 app.use('/product', proxy(URL_PRODUCT_SERVICE))
 app.use('/user', proxy(URL_USER_SERVICE))
 app.use('/customer', proxy(URL_CUSTOMER_SERVICE))
+app.use('/discount', proxy(URL_DISCOUNT_SERVICE))
+app.use('/order', proxy(URL_ORDER_SERVICE))
 
 app.listen(PORT || 3000, () => {
     console.log('http://localhost:' + (PORT || 3000));
